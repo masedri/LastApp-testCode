e5544df144b53df318d44f5c6d836878c9cf3292
masedri <marck.rodriguez09@gmail.com>
2023-06-27 00:24:00 +0200

    feat(layouts): add layouts default and detail

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col">
+    <UILastHeader />
+    <div class="flex flex-col gap-5 m-5">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
---
+++ layouts/detail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col h-screen relative">
+    <UILastHeader />
+
+    <slot name="header"></slot>
+    <div class="flex justify-between px-5 w-full absolute top-20">
+      <UILastIcon
+        name="arrow"
+        class="hover:scale-110 transition"
+        @click="$router.back()"
+      />
+      <div class="flex gap-2">
+        <UILastIcon
+          name="search"
+          class="bg-[#262638] opacity-80 rounded-full fill-white hover:scale-110 transition"
+          @click="$emit('openSearch')"
+        />
+        <UILastIcon name="fav" />
+      </div>
+    </div>
+    <div class="flex flex-col gap-5 relative m-5 h-full overflow-hidden">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineEmits<{ openSearch: [...payload: any[]] }>()
+</script>
+
+<style scoped></style>
