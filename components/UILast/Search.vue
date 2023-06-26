f48a850c0c037f7052fa5d35d828a10b48facc69
masedri <marck.rodriguez09@gmail.com>
2023-06-27 00:40:54 +0200

    feat(UILast): add Search component

---
+++ components/UILast/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-between gap-2 bg-[#F9F9FB] rounded-xl items-center px-4">
+    <UILastIcon
+      name="search"
+      class="fill-black"
+    />
+    <UILastInput
+      v-model="filter"
+      class="w-full bg-[#F9F9FB] h-[48px]"
+    ></UILastInput>
+    <UILastIcon
+      name="close"
+      class="hover:drop-shadow"
+      @click="$emit('close')"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+const filter = defineModel()
+defineEmits<{
+  close: [...playload: []]
+}>()
+</script>
