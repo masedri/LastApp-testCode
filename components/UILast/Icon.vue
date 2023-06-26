aa0e23f1d039f4e3c05dd56c21c742ce05342d61
masedri <marck.rodriguez09@gmail.com>
2023-06-27 00:36:59 +0200

    feat(UILast): add factory async component Icon

---
+++ components/UILast/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <component
+    :is="icon"
+    class="cursor-pointer"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+
+const props = defineProps<{
+  name: string
+}>()
+
+const icon = defineAsyncComponent(() => import(`../../assets/icons/${props.name.toLowerCase()}.svg?component`))
+</script>
